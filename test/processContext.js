13670d005c66123ea63de55618d15fd90fc2aebe
Mark Birbeck <mark.birbeck@gmail.com>
2017-07-30 22:42:29 +0100

    add ProcessContext to DirectRunner so that data can be processed and returned

---
+++ test/processContext.js
@@ -0,0 +1,38 @@
+const chai = require('chai');
+
+chai.use(require('chai-as-promised'));
+chai.should();
+
+const Pipeline = require('../lib/Pipeline');
+const ParDo = require('../lib/ParDo');
+const DoFn = require('../lib/DoFn');
+
+/**
+ * Define a DoFn for ParDo:
+ */
+
+class ComputeWordLengthFn extends DoFn {
+  processElement(c) {
+    let word = c.element();
+
+    c.output(word.length);
+  }
+}
+
+class OutputFn extends DoFn {
+  processElement(c) {
+    console.log(c.element());
+  }
+}
+
+describe('processElement', () => {
+  it('simple function', () => {
+    let p = Pipeline.create();
+
+    p
+    .apply(ParDo().of(new ComputeWordLengthFn()))
+    .apply(ParDo().of(new OutputFn()))
+    .run()
+    ;
+  });
+});
