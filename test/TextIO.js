25883d2e0c7779f053c4b96b4fc6cd40406ab814
Mark Birbeck <mark.birbeck@gmail.com>
2017-10-07 18:45:43 +0100

    add TextIO#Write

--- test/TextIO.js
+++ test/TextIO.js
@@ -4,6 +4,8 @@ const chai = require('chai');
 chai.should();
 
 const Pipeline = require('../lib/sdk/Pipeline');
+const Count = require('../lib/sdk/transforms/Count');
+const MapElements = require('../lib/sdk/transforms/MapElements');
 const ParDo = require('../lib/sdk/transforms/ParDo');
 const DoFn = require('../lib/sdk/transforms/DoFn');
 const TextIO = require('../lib/sdk/io/TextIO');
@@ -18,6 +20,32 @@ describe('TextIO', () => {
           c.element().should.eql('This is a simple file.\n');
         }
       }))
+      .run()
+      .waitUntilFinish()
+      ;
+    });
+  });
+
+  describe('write()', () => {
+    it('to()', () => {
+      return Pipeline.create()
+      .apply(TextIO.read().from(path.resolve(__dirname, './fixtures/file2.txt')))
+      .apply('ExtractWords', ParDo().of(
+        new class ExtractWordsFn extends DoFn {
+          processElement(c) {
+            c.element().split('\n').forEach(word => c.output(word));
+          }
+        }()
+      ))
+      .apply(Count.perElement())
+      .apply(MapElements.via(
+        new class extends DoFn {
+          apply(input) {
+            return `${input.getKey()}: ${input.getValue()}`;
+          }
+        }()
+      ))
+      .apply(TextIO.write().to('textio-write-wordcounts'))
       .run()
       .waitUntilFinish()
       ;
