aab48b97ab42876d9ec3fc96cc6a4e10833f2b8d
Mark Birbeck <mark.birbeck@gmail.com>
2018-03-23 13:28:24 +0000

    [#49] factor Split transform from test to own module

--- test/transforms/Split.js
+++ test/transforms/Split.js
@@ -7,51 +7,13 @@ const Pipeline = require('../../lib/sdk/Pipeline');
 const Create = require('../../lib/sdk/transforms/Create');
 const ParDo = require('../../lib/sdk/transforms/ParDo');
 const DoFn = require('../../lib/sdk/transforms/DoFn');
+const Split = require('../../lib/sdk/transforms/Split');
 
 describe('Split', () => {
   it('using \\n', async () => {
     await Pipeline.create()
     .apply(ParDo.of(Create.of(['line 1\nline 2\nline 3'])))
-    .apply('ExtractLines', ParDo.of(
-      new class ExtractLinesFn extends DoFn {
-        processStart() {
-          this.last = '';
-        }
-
-        processElement(c) {
-
-          /**
-           * Add any leftovers from previous processing to the front of
-           * the new data:
-           */
-
-          this.last += c.element();
-
-          /**
-           * Split the data:
-           */
-
-          const list = this.last.split('\n');
-
-          /**
-           * Save the very last entry for next time, since we don't know
-           * whether it's a full line or not:
-           */
-
-          this.last = list.pop();
-
-          /**
-           * Propagate all of the lines:
-           */
-
-          list.forEach(line => c.output(line));
-        }
-
-        processFinish(c) {
-          c.output(this.last);
-        }
-      }()
-    ))
+    .apply('Split', ParDo.of(new Split()))
     .apply(ParDo.of(new class extends DoFn {
       processStart() {
         this.result = [];
