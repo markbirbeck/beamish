c8d8255f1d654a380ac1063329fedc6466e37a35
Mark Birbeck <mark.birbeck@gmail.com>
2018-03-23 11:38:01 +0000

    [#49] add implementation of split using a DoFn

--- test/TextIO.js
+++ test/TextIO.js
@@ -5,6 +5,7 @@ chai.should();
 
 const Pipeline = require('../lib/sdk/Pipeline');
 const Count = require('../lib/sdk/transforms/Count');
+const Create = require('../lib/sdk/transforms/Create');
 const MapElements = require('../lib/sdk/transforms/MapElements');
 const ParDo = require('../lib/sdk/transforms/ParDo');
 const DoFn = require('../lib/sdk/transforms/DoFn');
@@ -18,6 +19,67 @@ describe('TextIO', () => {
       .apply(ParDo.of(new class extends DoFn {
         processElement(c) {
           c.element().should.eql('This is a simple file.\n');
+        }
+      }))
+      .run()
+      .waitUntilFinish()
+      ;
+    });
+
+    it('split()', () => {
+      return Pipeline.create()
+      .apply(ParDo.of(Create.of(['line 1\nline 2\nline 3'])))
+      .apply('ExtractLines', ParDo.of(
+        new class ExtractLinesFn extends DoFn {
+          processStart() {
+            this.last = '';
+          }
+
+          processElement(c) {
+
+            /**
+             * Add any leftovers from previous processing to the front of
+             * the new data:
+             */
+
+            this.last += c.element();
+
+            /**
+             * Split the data:
+             */
+
+            const list = this.last.split(/\n/);
+
+            /**
+             * Save the very last entry for next time, since we don't know
+             * whether it's a full line or not:
+             */
+
+            this.last = list.pop();
+
+            /**
+             * Propagate all of the lines:
+             */
+
+            list.forEach(line => c.output(line));
+          }
+
+          processFinish(c) {
+            c.output(this.last);
+          }
+        }()
+      ))
+      .apply(ParDo.of(new class extends DoFn {
+        processStart() {
+          this.result = [];
+        }
+
+        processElement(c) {
+          this.result.push(c.element());
+        }
+
+        processFinish() {
+          this.result.should.eql(['line 1', 'line 2', 'line 3']);
         }
       }))
       .run()
