9e39081d4dace55dd9de3f295b56ade25d1d0778
Mark Birbeck <mark.birbeck@gmail.com>
2018-03-23 14:08:43 +0000

    [#49] group delimiter tests in Split transform tests

--- test/transforms/Split.js
+++ test/transforms/Split.js
@@ -10,69 +10,71 @@ const DoFn = require('../../lib/sdk/transforms/DoFn');
 const Split = require('../../lib/sdk/transforms/Split');
 
 describe('Split', () => {
-  it('using \\n', async () => {
-    await Pipeline.create()
-    .apply(ParDo.of(Create.of(['line 1\nline 2\nline 3'])))
-    .apply('Split', ParDo.of(new Split()))
-    .apply(ParDo.of(new class extends DoFn {
-      processStart() {
-        this.result = [];
-      }
+  describe('delimiters', () => {
+    it('using \\n', async () => {
+      await Pipeline.create()
+      .apply(ParDo.of(Create.of(['line 1\nline 2\nline 3'])))
+      .apply('Split', ParDo.of(new Split()))
+      .apply(ParDo.of(new class extends DoFn {
+        processStart() {
+          this.result = [];
+        }
 
-      processElement(c) {
-        this.result.push(c.element());
-      }
+        processElement(c) {
+          this.result.push(c.element());
+        }
 
-      processFinish() {
-        this.result.should.eql(['line 1', 'line 2', 'line 3']);
-      }
-    }))
-    .run()
-    .waitUntilFinish()
-    ;
-  });
+        processFinish() {
+          this.result.should.eql(['line 1', 'line 2', 'line 3']);
+        }
+      }))
+      .run()
+      .waitUntilFinish()
+      ;
+    });
 
-  it('using \\r', async () => {
-    await Pipeline.create()
-    .apply(ParDo.of(Create.of(['line 4\rline 5\rline 6'])))
-    .apply('Split', ParDo.of(new Split()))
-    .apply(ParDo.of(new class extends DoFn {
-      processStart() {
-        this.result = [];
-      }
+    it('using \\r', async () => {
+      await Pipeline.create()
+      .apply(ParDo.of(Create.of(['line 4\rline 5\rline 6'])))
+      .apply('Split', ParDo.of(new Split()))
+      .apply(ParDo.of(new class extends DoFn {
+        processStart() {
+          this.result = [];
+        }
 
-      processElement(c) {
-        this.result.push(c.element());
-      }
+        processElement(c) {
+          this.result.push(c.element());
+        }
 
-      processFinish() {
-        this.result.should.eql(['line 4', 'line 5', 'line 6']);
-      }
-    }))
-    .run()
-    .waitUntilFinish()
-    ;
-  });
+        processFinish() {
+          this.result.should.eql(['line 4', 'line 5', 'line 6']);
+        }
+      }))
+      .run()
+      .waitUntilFinish()
+      ;
+    });
 
-  it('using \\r\\n', async () => {
-    await Pipeline.create()
-    .apply(ParDo.of(Create.of(['line 7\r\nline 8\r\nline 9'])))
-    .apply('Split', ParDo.of(new Split()))
-    .apply(ParDo.of(new class extends DoFn {
-      processStart() {
-        this.result = [];
-      }
+    it('using \\r\\n', async () => {
+      await Pipeline.create()
+      .apply(ParDo.of(Create.of(['line 7\r\nline 8\r\nline 9'])))
+      .apply('Split', ParDo.of(new Split()))
+      .apply(ParDo.of(new class extends DoFn {
+        processStart() {
+          this.result = [];
+        }
 
-      processElement(c) {
-        this.result.push(c.element());
-      }
+        processElement(c) {
+          this.result.push(c.element());
+        }
 
-      processFinish() {
-        this.result.should.eql(['line 7', 'line 8', 'line 9']);
-      }
-    }))
-    .run()
-    .waitUntilFinish()
-    ;
+        processFinish() {
+          this.result.should.eql(['line 7', 'line 8', 'line 9']);
+        }
+      }))
+      .run()
+      .waitUntilFinish()
+      ;
+    });
   });
 });
