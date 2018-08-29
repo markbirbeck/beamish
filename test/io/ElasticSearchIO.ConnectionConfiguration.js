a59b7656797b46d95dc3713555457733e3423482
Mark Birbeck <mark.birbeck@gmail.com>
2018-08-29 12:23:12 +0100

    [#62] Add argument checking when creating ES configuration

--- test/io/ElasticSearchIO.ConnectionConfiguration.js
+++ test/io/ElasticSearchIO.ConnectionConfiguration.js
@@ -8,3 +8,17 @@ const config = ConnectionConfiguration.create('http://host:9200', 'my-index', 'm
 tap.equal(config.addresses, 'http://host:9200')
 tap.equal(config.index, 'my-index')
 tap.equal(config.type, 'my-type')
+
+tap.comment('Check that errors are thrown if arguments are incorrect.')
+tap.throws(
+  () => ConnectionConfiguration.create('http://host:9200', 'my-index'),
+  { message: 'type can not be undefined' }
+)
+tap.throws(
+  () => ConnectionConfiguration.create('http://host:9200'),
+  { message: 'index can not be undefined' }
+)
+tap.throws(
+  () => ConnectionConfiguration.create(),
+  { message: 'addresses can not be undefined' }
+)
