f9db0024a4eb73c11b2bbb0d312458af52420c52
Mark Birbeck <mark.birbeck@gmail.com>
2018-08-29 11:24:11 +0100

    [#63] Factor checkArgument to own module

--- test/util/Preconditions.js
+++ test/util/Preconditions.js
@@ -1,10 +1,6 @@
 const tap = require('tap')
 
-const checkArgument = (expression, errorMessage) => {
-  if (!expression) {
-    throw new Error(errorMessage)
-  }
-}
+const { checkArgument } = require('../../lib/sdk/util/Preconditions')
 
 tap.comment('Check that the assertions are the right way around.')
 tap.doesNotThrow(() => checkArgument('foo' !== null))
