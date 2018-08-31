61c9e7aa0139b7dfaa73bf6491f4ce5b287822be
Mark Birbeck <mark.birbeck@gmail.com>
2018-08-31 12:34:36 +0100

    [#65] Add failing test for requestTimeout as a configuration option

--- test/io/ElasticSearchIO.ConnectionConfiguration.js
+++ test/io/ElasticSearchIO.ConnectionConfiguration.js
@@ -3,11 +3,12 @@ const tap = require('tap')
 const ConnectionConfiguration = require('../../lib/sdk/io/ElasticSearchIO/ConnectionConfiguration')
 
 tap.comment('Check that all values are set correctly.')
-const config = ConnectionConfiguration.create('http://host:9200', 'my-index', 'my-type')
+let config = ConnectionConfiguration.create('http://host:9200', 'my-index', 'my-type')
 
 tap.equal(config.addresses, 'http://host:9200')
 tap.equal(config.index, 'my-index')
 tap.equal(config.type, 'my-type')
+tap.equal(config.requestTimeout, undefined)
 
 tap.comment('Check that errors are thrown if arguments are incorrect.')
 tap.throws(
@@ -22,3 +23,11 @@ tap.throws(
   () => ConnectionConfiguration.create(),
   { message: 'addresses can not be undefined' }
 )
+
+tap.comment('Check requestTimeout (it\'s optional)')
+config = ConnectionConfiguration.create('http://host:9200', 'my-index', 'my-type', 10000)
+
+tap.equal(config.addresses, 'http://host:9200')
+tap.equal(config.index, 'my-index')
+tap.equal(config.type, 'my-type')
+tap.equal(config.requestTimeout, 10000)
