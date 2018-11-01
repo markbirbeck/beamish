3182a77d1b2bc1082c58b11d5eb10692b0d70ae6
Mark Birbeck <mark.birbeck@gmail.com>
2018-11-01 13:37:32 +0000

    [#92] Factor DoFnAsReadable to own module

--- test/tap/Pipeline/node-streams/node-streams.js
+++ test/tap/Pipeline/node-streams/node-streams.js
@@ -8,6 +8,7 @@ const zlib = require('zlib')
 const pipeline = util.promisify(stream.pipeline)
 
 const DoFn = require('./../../../../lib/sdk/harnesses/node-streams/DoFn')
+const DoFnAsReadable = require('./../../../../lib/sdk/harnesses/node-streams/DoFnAsReadable')
 const DoFnAsTransform = require('./../../../../lib/sdk/harnesses/node-streams/DoFnAsTransform')
 
 class SplitNewLineFn extends DoFn {
@@ -58,52 +59,6 @@ class CountFn extends DoFn {
 
   finalElement(c) {
     c.output('' + this.count)
-  }
-}
-
-class DoFnAsReadable extends stream.Readable {
-  constructor(fn) {
-    super()
-    this.fn = fn
-    this.setupComplete = false
-    this.teardownComplete = false
-  }
-
-  /**
-   * When the upstream handler is ready for more, then unpause the
-   * wrapped stream. On first pass through set up event handlers:
-   */
-
-  _read() {
-    if (!this.setupComplete) {
-      this.setupComplete = true
-      if (this.fn.setup) {
-        this.fn.setup()
-      }
-
-      /**
-       * When there is no more data, let the upstream handler
-       * know, and ask the downstream handler to destroy itself:
-       */
-
-      this.fn.stream.on('end', () => {
-        this.push(null)
-        if (!this.teardownComplete) {
-          this.teardownComplete = true
-          if (this.fn.teardown) {
-            this.fn.teardown()
-          }
-        }
-      })
-
-      this.fn.stream.on('data', chunk => {
-        if (!this.push(chunk)) {
-          this.fn.stream.pause()
-        }
-      })
-    }
-
-    this.fn.stream.resume()
   }
 }
 
