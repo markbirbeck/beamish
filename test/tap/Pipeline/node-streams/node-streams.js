c9127368a14623bd2af1b9798c445e1cbe696667
Mark Birbeck <mark.birbeck@gmail.com>
2018-11-01 01:07:39 +0000

    [#92] Add a transform that splits a stream into lines

--- test/tap/Pipeline/node-streams/node-streams.js
+++ test/tap/Pipeline/node-streams/node-streams.js
@@ -7,16 +7,51 @@ const zlib = require('zlib')
 
 const pipeline = util.promisify(stream.pipeline)
 
-class IdentityTransform extends stream.Transform {
+class SplitNewLineTransform extends stream.Transform {
   _transform(chunk, encoding, callback) {
-    this.push(chunk)
+    if (this.last === undefined) {
+      this.last = ''
+    }
+
+    /**
+     * Add any leftovers from previous processing to the front of
+     * the new data:
+     */
+
+    if (encoding === 'buffer') {
+      this.last += chunk.toString()
+    } else {
+      this.last += chunk
+    }
+
+    /**
+     * Split the data; we're looking for '\r', '\n', or '\r\n':
+     */
+
+    const list = this.last.split(/\r\n|[\r\n]/)
+
+    /**
+     * Save the very last entry for next time, since we don't know
+     * whether it's a full line or not:
+     */
+
+    this.last = list.pop()
+
+    while (list.length) {
+      this.push(list.shift())
+    }
+    callback()
+  }
+
+  _flush(callback) {
+    this.push(this.last)
     callback()
   }
 }
 
 async function main() {
   const source = fs.createReadStream('../../../fixtures/shakespeare/1kinghenryiv')
-  const transform = new IdentityTransform()
+  const transform = new SplitNewLineTransform()
   const sink = fs.createWriteStream('../../../fixtures/output/1kinghenryiv')
 
   try {
@@ -29,7 +64,7 @@ async function main() {
     console.log('Pipeline succeeded')
 
     const stat = fs.statSync('../../../fixtures/output/1kinghenryiv')
-    tap.same(stat.size, 145002)
+    tap.same(stat.size, 140533)
   } catch (err) {
     console.error('Pipeline failed', err)
   }
