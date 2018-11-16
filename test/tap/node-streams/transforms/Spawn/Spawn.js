0cb16d0e6ba51c4aa9d791bdd1f321ddab650839
Mark Birbeck <mark.birbeck@gmail.com>
2018-11-16 11:41:03 +0000

    [#97] Add direct use of 'duplex-child-process' in pipeline

---
+++ test/tap/node-streams/transforms/Spawn/Spawn.js
@@ -0,0 +1,49 @@
+const tap = require('tap')
+tap.comment('SpawnFn')
+
+const path = require('path')
+const ChildProcess = require('duplex-child-process')
+
+const DoFn = require('./../../../../../lib/sdk/harnesses/node-streams/DoFn')
+const FileWriterFn = require('./../../../../../lib/sdk/io/node-streams/FileWriterFn')
+const CreateReaderFn = require('./../../../../../lib/sdk/io/node-streams/CreateReaderFn')
+const ParDo = require('./../../../../../lib/sdk/harnesses/node-streams/ParDo')
+const Pipeline = require('./../../../../../lib/sdk/NodeStreamsPipeline')
+
+/**
+ * Define a DoFn for ParDo:
+ */
+
+class AddCrFn extends DoFn {
+  processElement(c) {
+    c.output(`${c.element().toString()}\n`)
+  }
+}
+
+const main = async () => {
+  const p = Pipeline.create()
+
+  p
+  .apply(ParDo.of(new CreateReaderFn(['hello', 'world!'])))
+  .apply(ParDo.of(new AddCrFn()))
+  .apply(new ChildProcess.spawn('tr', ['[a-z]', '[A-Z]']))
+  .apply(
+    ParDo.of(new class extends DoFn {
+      processElement(c) {
+        c.output(
+          tap.same(c.element().toString(), 'HELLO\nWORLD!\n').toString()
+        )
+      }
+    })
+  )
+  .apply(
+    ParDo.of(new FileWriterFn(path.resolve(__dirname,
+      '../../../../fixtures/output/spawn-tr')))
+  )
+
+  return p
+  .run()
+  .waitUntilFinish()
+}
+
+tap.resolves(main())
