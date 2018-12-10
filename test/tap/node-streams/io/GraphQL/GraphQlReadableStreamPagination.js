4256c58dc9dad1241f8ec8976aedc99a8cbe7ba9
Mark Birbeck <mark.birbeck@gmail.com>
2018-12-10 23:34:44 +0000

    [#104] Reuse same query object for both stages of cursor-based query

--- test/tap/node-streams/io/GraphQL/GraphQlReadableStreamPagination.js
+++ test/tap/node-streams/io/GraphQL/GraphQlReadableStreamPagination.js
@@ -153,3 +153,130 @@ tap.test('two stages, different queries', async t => {
   t.notSame(list1, list2)
   t.end()
 })
+
+tap.test('two stages, one query', async t => {
+  /**
+   * Publically available GraphQL endpoint for MusicBrainz:
+   */
+  const url = 'https://graphbrainz.herokuapp.com/'
+  const outputPath = path.resolve(__dirname,
+    '../../../../fixtures/output/GraphQlReadableStream')
+
+  const query = gql`
+    query GoldenLabels($cursor: String) {
+      search {
+        labels(query: "Golden", first: 5, after: $cursor) {
+          ...labelResults
+        }
+      }
+    }
+
+    fragment labelResults on LabelConnection {
+      pageInfo {
+        endCursor
+      }
+      edges {
+        cursor
+        node {
+          name
+        }
+      }
+    }
+  `
+
+  /**
+   * The first step runs a query to get a list, and also gets a
+   * cursor which can be used to retrieve more results:
+   */
+  let cursor
+  let list1
+
+  let p = Pipeline.create()
+  p
+  .apply(
+    new GraphQlReadableStream({
+      url,
+      options: {
+        query
+      }
+    })
+  )
+  .apply(ParDo.of(new class extends DoFn {
+    processElement(c) {
+      const input = c.element()
+
+      /**
+       * Save the cursor for the next pipeline:
+       */
+      cursor = input.search.labels.pageInfo.endCursor
+
+      /**
+       * Save the names in alphabetical order:
+       */
+      list1 = input.search.labels.edges.map(edge => edge.node.name)
+      list1.sort()
+
+      /**
+       * Check that we have the right number of results:
+       */
+      c.output(
+        t.same(
+          input.search.labels.edges.length,
+          5
+        ).toString()
+      )
+    }
+  }))
+  .apply(ParDo.of(new FileWriterFn(outputPath)))
+  await p.run().waitUntilFinish()
+
+  /**
+   * The next pipeline uses the cursor from the previous
+   * pipeline to get another list. Note that the only difference
+   * between this query and the previous one is the presence
+   * of the 'cursor' variable:
+   */
+  let list2
+  p = Pipeline.create()
+  p
+  .apply(
+    new GraphQlReadableStream({
+      url,
+      options: {
+        query,
+        variables: {
+          cursor
+        }
+      }
+    })
+  )
+  .apply(ParDo.of(new class extends DoFn {
+    processElement(c) {
+      const input = c.element()
+
+      /**
+       * Save the names in alphabetical order:
+       */
+      list2 = input.search.labels.edges.map(edge => edge.node.name)
+      list2.sort()
+
+      /**
+       * Check that we have the right number of results:
+       */
+      c.output(
+        t.same(
+          input.search.labels.edges.length,
+          5
+        ).toString()
+      )
+    }
+  }))
+  .apply(ParDo.of(new FileWriterFn(outputPath)))
+  await p.run().waitUntilFinish()
+
+  /**
+   * Ensure that the two lists are different:
+   */
+  t.notSame(list1, list2)
+  t.end()
+})
