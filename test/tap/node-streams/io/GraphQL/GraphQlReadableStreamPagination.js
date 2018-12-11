2e2c83260fce1846861fa25c5845605012f291e2
Mark Birbeck <mark.birbeck@gmail.com>
2018-12-11 01:00:42 +0000

    [#104] Add cursor handling to GraphQL readable stream

--- test/tap/node-streams/io/GraphQL/GraphQlReadableStreamPagination.js
+++ test/tap/node-streams/io/GraphQL/GraphQlReadableStreamPagination.js
@@ -4,6 +4,7 @@ tap.comment('GraphQlReadableStreamPagination')
 const path = require('path')
 const gql = require('graphql-tag')
 
+const Count = require('./../../../../../lib/sdk/transforms/node-streams/Count')
 const DoFn = require('./../../../../../lib/sdk/harnesses/node-streams/DoFn')
 const FileWriterFn = require('./../../../../../lib/sdk/io/node-streams/FileWriterFn')
 const ParDo = require('./../../../../../lib/sdk/harnesses/node-streams/ParDo')
@@ -278,5 +279,78 @@ tap.test('two stages, one query', async t => {
    * Ensure that the two lists are different:
    */
   t.notSame(list1, list2)
+  t.end()
+})
+
+tap.test('GraphQlReadableStream handles pagination', async t => {
+  /**
+   * Publically available GraphQL endpoint for MusicBrainz:
+   */
+  const url = 'https://graphbrainz.herokuapp.com/'
+  const outputPath = path.resolve(__dirname,
+    '../../../../fixtures/output/GraphQlReadableStream')
+
+  const query = gql`
+    query HarpLabels($cursor: String) {
+      search {
+        labels(query: "Harp", first: 5, after: $cursor) {
+          ...labelResults
+        }
+      }
+    }
+
+    fragment labelResults on LabelConnection {
+      pageInfo {
+        endCursor
+      }
+      edges {
+        cursor
+        node {
+          name
+        }
+      }
+    }
+  `
+
+  let p = Pipeline.create()
+  p
+  .apply(
+    new GraphQlReadableStream({
+      url,
+      options: {
+        query
+      },
+      cursorPath: 'search.labels.pageInfo.endCursor',
+      size: 10 / 5
+    })
+  )
+  .apply(ParDo.of(new class extends DoFn {
+    processElement(c) {
+      const input = c.element()
+
+      for (const edge of input.search.labels.edges) {
+        c.output(edge)
+      }
+    }
+  }))
+  .apply(Count.globally())
+  .apply(ParDo.of(new class extends DoFn {
+    processElement(c) {
+      const input = c.element()
+
+      /**
+       * Check that we have the right number of results:
+       */
+      c.output(
+        t.same(
+          input,
+          10
+        ).toString()
+      )
+    }
+  }))
+  .apply(ParDo.of(new FileWriterFn(outputPath)))
+  await p.run().waitUntilFinish()
+
   t.end()
 })
