995c0d59712ff932e8cc1915cb0994988a8de366
Mark Birbeck <mark.birbeck@gmail.com>
2019-01-23 18:38:04 +0000

    [#92] Add Create.of() using new Node stream version

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const DoFn = require('./lib/sdk/harnesses/node-streams/DoFn')
 const FileWriterFn = require('./lib/sdk/io/node-streams/FileWriterFn')
-const CreateReaderFn = require('./lib/sdk/io/node-streams/CreateReaderFn')
+const Create = require('./lib/sdk/transforms/node-streams/Create')
 const ParDo = require('./lib/sdk/harnesses/node-streams/ParDo')
 const Pipeline = require('./lib/sdk/NodeStreamsPipeline')
 
 module.exports = {
   DoFn,
   FileWriterFn,
-  CreateReaderFn,
+  Create,
   ParDo,
   Pipeline
 }
