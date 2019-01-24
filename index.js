8abfc9678bb82cde826f11c7a43a91afa2841d4e
Mark Birbeck <mark.birbeck@gmail.com>
2019-01-24 14:33:28 +0000

    Put imports in index.js in alphabetical order

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
+const Create = require('./lib/sdk/transforms/node-streams/Create')
 const DoFn = require('./lib/sdk/harnesses/node-streams/DoFn')
 const FileWriterFn = require('./lib/sdk/io/node-streams/FileWriterFn')
-const Create = require('./lib/sdk/transforms/node-streams/Create')
 const NoopWriterFn = require('./lib/sdk/io/node-streams/NoopWriterFn')
 const ParDo = require('./lib/sdk/harnesses/node-streams/ParDo')
 const Pipeline = require('./lib/sdk/NodeStreamsPipeline')
 
 module.exports = {
+  Create,
   DoFn,
   FileWriterFn,
-  Create,
   NoopWriterFn,
   ParDo,
   Pipeline
