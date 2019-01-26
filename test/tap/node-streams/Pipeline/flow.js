ba230657aa10c0612db2e6cfe6422b0258b443e5
Mark Birbeck <mark.birbeck@gmail.com>
2019-01-26 13:43:14 +0000

    [#113] Use name parameters in flow test pipeline

--- test/tap/node-streams/Pipeline/flow.js
+++ test/tap/node-streams/Pipeline/flow.js
@@ -29,85 +29,101 @@ const main = async () => {
 
   /**
    * Generate a couple of records with a timestamp:
+   *
+   * TODO(MRB): The initial Create.of() does nothing other than pass the test imposed
+   * by the pipeline that the first step must be a readable stream. There is probably
+   * a better way around this.
    */
 
   p
   .apply(Create.of(['']))
-  .apply(ParDo.of(
-    new class extends DoFn {
-      async processElement(c) {
-        for (let i = 0; i < 2; i++) {
-          await c.output({
-            ts1: new Date()
-          })
+  .apply(
+    'GenerateTimestamps',
+    ParDo.of(
+      new class extends DoFn {
+        async processElement(c) {
+          for (let i = 0; i < 2; i++) {
+            await c.output({
+              ts1: new Date()
+            })
+          }
         }
       }
-    }
-  ))
+    )
+  )
 
   /**
    * Add another timestamp to each record. We want to be adding this second
    * timestamp to record 1 *before* the previous step generates record 2:
    */
 
-  .apply(ParDo.of(
-    new class extends DoFn {
-      processElement(c) {
-        const input = c.element()
-        input.ts2 = new Date()
-        c.output(input)
+  .apply(
+    'AddExtraTimestamp',
+    ParDo.of(
+      new class extends DoFn {
+        processElement(c) {
+          const input = c.element()
+          input.ts2 = new Date()
+          c.output(input)
+        }
       }
-    }
-  ))
+    )
+  )
 
   /**
    * Accumulate the results into one object so that we can compare the
    * timestamps:
    */
 
-  .apply(ParDo.of(
-    new class extends DoFn {
-      setup() {
-        this.results = []
-      }
+  .apply(
+    'AccumulateAllResults',
+    ParDo.of(
+      new class extends DoFn {
+        setup() {
+          this.results = []
+        }
 
-      processElement(c) {
-        this.results.push(c.element())
-      }
+        processElement(c) {
+          this.results.push(c.element())
+        }
 
-      processFinish(pe) {
-        pe.output(this.results)
+        processFinish(pe) {
+          pe.output(this.results)
+        }
       }
-    }
-  ))
+    )
+  )
 
   /**
    * Ensure that the second timestamp for the first record is *before*
    * the first timestamp for the second record:
    */
 
-  .apply(ParDo.of(
-    new class extends DoFn {
-      apply(input) {
+  .apply(
+    'TestTimestampSequence',
+    ParDo.of(
+      new class extends DoFn {
+        apply(input) {
 
-        /**
-         * TODO(MB): Would be quite simple to set up some kind of 'test' class
-         * that already has tap on it. Maybe the class would expose all of the
-         * same methods that tap has, so that we could do things like:
-         *
-         *  .apply(ParDo.of(
-         *    new class extends TestDoFn {
-         *      equal() {
-         *        return 77
-         *      }
-         *    }
-         *  ))
-         */
+          /**
+           * TODO(MB): Would be quite simple to set up some kind of 'test' class
+           * that already has tap on it. Maybe the class would expose all of the
+           * same methods that tap has, so that we could do things like:
+           *
+           *  .apply(ParDo.of(
+           *    new class extends TestDoFn {
+           *      equal() {
+           *        return 77
+           *      }
+           *    }
+           *  ))
+           */
 
-        return tap.ok(input[0].ts2 < input[1].ts1).toString()
+          return tap.ok(input[0].ts2 < input[1].ts1).toString()
+        }
       }
-    }
-  ))
+    )
+  )
   .apply('Noop', ParDo.of(new NoopWriterFn()))
 
   return p
